ebeb73cc502f81580a401cbade11791d7e9cf5db
zp_time <190265950@qq.com>
2021-01-22 14:41:45 +0800

    修复主项目访问子项目找不到子项目跑出的钩子问题

--- src/main.js
+++ src/main.js
@@ -10,12 +10,12 @@ const render = (props) => {
     render: h => h(App)
   }).$mount('#vue-app');
 }
-
+console.log(window.__POWERED_BY_QIANKUN__)
 if (window.__POWERED_BY_QIANKUN__) {
   __webpack_public_path__ = window.__INJECTED_PUBLIC_PATH_BY_QIANKUN__
 }
 if (!window.__POWERED_BY_QIANKUN__) { // 默认独立运行
-  render();
+  render()
 }
 
 // 子组件的协议就ok了
--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,9 @@
 
 const Timestamp = new Date().getTime();
 const isPro = process.env.NODE_ENV === 'production'
-const publicPath = isPro ? '//8.129.230.32/:8081/vue/' : `//192.168.0.69:9000/vue/`;
+const publicPath = isPro ? '//8.129.230.32/:8081/vue/' : `//192.168.0.36:9000/vue/`;
+const CompressionPlugin = require('compression-webpack-plugin')
+const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin
 
 module.exports = {
   // publicPath: isPro ? '/vue/' : './', // 公共路径
@@ -15,6 +17,7 @@ module.exports = {
       'Access-Control-Allow-Origin': '*' // 微前端跨域处理
     }
   },
+
   configureWebpack: {
     output: { // 输出文件配置
       library: 'vue', // 输出后的文件名称
@@ -24,29 +27,62 @@ module.exports = {
       globalObject: 'this'
     }
   },
-  chainWebpack: (config) => {
-    config.module
-      .rule('fonts')
+
+  // chainWebpack: (config) => {
+  //   config.module
+  //     .rule('fonts')
+  //     .use('url-loader')
+  //     .loader('url-loader')
+  //     .options({})
+  //     .end()
+  //   config.module
+  //     .rule('images')
+  //     .use('url-loader')
+  //     .loader('url-loader')
+  //     .options({})
+  //     .end()
+  // },
+
+  chainWebpack: config => {
+    if (process.env.NODE_ENV === 'production') {
+      // 给js和css配置版本号
+      config.output.filename('static/js/[name].' + Timestamp + '.js').end();
+      config.output.chunkFilename('static/js/[name].' + Timestamp + '.js').end();
+      config.plugin('extract-css').tap(args => [{
+        filename: `static/css/[name].${Timestamp}.css`,
+        chunkFilename: `static/css/[name].${Timestamp}.css`
+      }])
+    }
+    config.module.rule('images')
       .use('url-loader')
       .loader('url-loader')
-      .options({})
-      .end()
-    config.module
-      .rule('images')
-      .use('url-loader')
-      .loader('url-loader')
-      .options({})
-      .end()
-  }
-  // chainWebpack: config => {
-  //   if (process.env.NODE_ENV === 'production') {
-  //     // 给js和css配置版本号
-  //     config.output.filename('static/js/[name].' + Timestamp + '.js').end();
-  //     config.output.chunkFilename('static/js/[name].' + Timestamp + '.js').end();
-  //     config.plugin('extract-css').tap(args => [{
-  //       filename: `static/css/[name].${Timestamp}.css`,
-  //       chunkFilename: `static/css/[name].${Timestamp}.css`
-  //     }])
-  //   }
+      .options({
+        limit: 4096, // 小于4kb将会被打包成 base64
+        fallback: {
+          loader: 'file-loader',
+          options: {
+            name: 'img/[name].[hash:8].[ext]',
+            publicPath
+          }
+        }
+      })
+  },
+  // configureWebpack: config => {
+  //   config.plugins.push(
+  //     new CompressionPlugin({
+  //       algorithm: 'gzip', // 使用gzip压缩
+  //       test: /\.js$|\.html$|\.css$/, // 匹配文件名
+  //       filename: '[path].gz[query]', // 压缩后的文件名(保持原文件名，后缀加.gz)
+  //       minRatio: 1, // 压缩率小于1才会压缩
+  //       threshold: 10240, // 对超过10k的数据压缩
+  //       deleteOriginalAssets: false // 是否删除未压缩的源文件，谨慎设置，如果希望提供非gzip的资源，可不设置或者设置为false（比如删除打包后的gz后还可以加载到原始资源文件）
+  //     })
+  //   )
+  //   // if (process.env.NODE_ENV === 'production') {
+  //   //   config.plugins.push(
+  //   //     new BundleAnalyzerPlugin({
+  //   //       analyzerPort: 8174
+  //   //     })
+  //   //   )
   // }
 }
